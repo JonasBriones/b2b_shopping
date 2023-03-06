dca28875a774e8c0ccf5a04eb2020d87c61b547b
Jonas <jonathan.brioneslavados@cencosud.cl>
2023-03-06 10:23:19 -0300

    modificaciones en varias partes

---
+++ app/routes/privado/externo/empresa/index.tsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+const Empresa: React.FC = () => {
+    return (
+        <React.Fragment>
+            Empresa
+        </React.Fragment>
+    )
+}
+
+export default Empresa;
--- app/src/components/ui/layouts/layout.tsx
+++ app/src/components/ui/layouts/layout.tsx
@@ -4,8 +4,7 @@ import React from 'react';
 const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<React.Fragment>
-			<div
-			>
+			<div>
 				<Box
 					display="flex"
 					justifyContent="center"
@@ -20,4 +19,3 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default Layout;
-
